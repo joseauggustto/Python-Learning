409940005b7415a86fa140151e3abc9b8215f9c3
José Augusto <jose.morais22aluno@gmail.com>
2026-08-28 22:52:59 -0300

    More learning

--- Projetos/extratordecodes.py
+++ Projetos/extratordecodes.py
@@ -1,13 +1,15 @@
 def pin_extractor(poems):
 
-    secret_codes = []
-    for poem in poems:
-        secret_code = ''
-        lines = poem.split('\n')
-        for line_index, line in enumerate(lines):
-            words = line.split()
-            if len(words) > line_index:
-                secret_code += str(len(words[line_index]))
+    secret_codes = []  
+
+    for poem in poems:      
+        secret_code = ''  
+        lines = poem.split('\n') 
+
+        for line_index, line in enumerate(lines):        
+            words = line.split() 
+            if len(words) > line_index:      
+                secret_code += str(len(words[line_index])) 
             else:
                 secret_code += '0'
         secret_codes.append(secret_code)
--- Teoria/cmdimport-teste.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-print(f"Versão python: {sys.version}")
-print(f"Versão do sistema operacional: {os.name}")
-print(f"Executável em uso: {sys.executable}")
-
